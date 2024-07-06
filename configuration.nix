188daa6289599e6fc1ac470d52745db4145df020
Louis Taylor <louis@kragniz.eu>
2024-07-06 16:41:37 +0100

    Add some nvim config

--- configuration.nix
+++ configuration.nix
@@ -56,6 +56,52 @@
     defaultEditor = true;
     viAlias = true;
     vimAlias = true;
+    configure = {
+      customRC = ''
+        colorscheme rose-pine-dawn
+        set number
+        set expandtab
+        set autoindent
+        set tabstop=4
+        set shiftwidth=4
+        set list
+        set listchars=tab:→\ ,nbsp:␣,trail:•
+
+        lua <<EOF
+          local cmp = require'cmp'
+
+          cmp.setup({
+            snippet = {
+              expand = function(args)
+                vim.snippet.expand(args.body)
+              end,
+            },
+            mapping = cmp.mapping.preset.insert({
+              ['<C-b>'] = cmp.mapping.scroll_docs(-4),
+              ['<C-f>'] = cmp.mapping.scroll_docs(4),
+              ['<C-Space>'] = cmp.mapping.complete(),
+              ['<C-e>'] = cmp.mapping.abort(),
+              ['<CR>'] = cmp.mapping.confirm({ select = true }), -- Accept currently selected item. Set `select` to `false` to only confirm explicitly selected items.
+            }),
+            sources = cmp.config.sources({
+              { name = 'nvim_lsp' },
+            }, {
+              { name = 'buffer' },
+            })
+          })
+        EOF
+      '';
+      packages.myVimPackage = with pkgs.vimPlugins; {
+        start = [
+          ctrlp
+          rose-pine
+          nvim-treesitter
+          rustaceanvim
+          nvim-cmp
+          cmp-nvim-lsp
+        ];
+      };
+    };
   };
 
   environment.systemPackages = with pkgs; [
